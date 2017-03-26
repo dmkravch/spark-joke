904db73a3c94a74fae9fb935a2437b58e786b91e
dmkravch <dmkravch@cisco.com>
2017-03-26 22:08:43 +0200

    TO some files

--- com/ciscospark/call.java
+++ com/ciscospark/call.java
@@ -3,10 +3,11 @@ package com.ciscospark;
 import java.net.URI;
 import java.util.Arrays;
 
+
 import com.ciscospark.*;
 
 class call {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // To obtain a developer access token, visit http://developer.ciscospark.com
         String accessToken = "NDdiMDk1YTYtZGZhYy00MzYyLThhMmQtYTA4NzA1OTVmODYxMTA5MWJhODItMTM4";
 
@@ -15,7 +16,7 @@ class call {
                 .baseUrl(URI.create("https://api.ciscospark.com/v1"))
                 .accessToken(accessToken)
                 .build();
-
+        String b = com.ciscospark.GetJoke.getHTML();
         // List the rooms that I'm in
   /**      spark.rooms()
                 .iterate()
@@ -27,15 +28,15 @@ class call {
         spark.rooms().iterate().forEachRemaining(room -> {
         	System.out.println(room.getTitle());
         });
- */
+       Room room = new Room();
+        room.setId("Y2lzY29zcGFyazovL3VzL1JPT00vYWNhYzZmNzQtNWVlZi0zNmE1LWJhNjgtNmFiNzg5NmIyMjBl");
+        
+  */       
         Message message = new Message();
         message.setRoomId("Y2lzY29zcGFyazovL3VzL1JPT00vYWNhYzZmNzQtNWVlZi0zNmE1LWJhNjgtNmFiNzg5NmIyMjBl");
-        message.setText("Hello World!");
+        message.setText(b);
         spark.messages().post(message);
-        /**       
-        Room room = new Room();
-        room.setId("Y2lzY29zcGFyazovL3VzL1JPT00vYWNhYzZmNzQtNWVlZi0zNmE1LWJhNjgtNmFiNzg5NmIyMjBl");
-   */     
+                 
         
         
         	
